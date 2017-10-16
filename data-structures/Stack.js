1864f052d9c7cee11729931329bd8639e53426c5
Yahor Simak <simak_yahor@mail.ru>
2017-10-16 22:56:34 +0300

    add empty abstraction of Stack data structure

---
+++ data-structures/Stack.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  // delete the top element of stack
+  pop() {
+
+  }
+
+  // add element on top of stack
+  push() {
+
+  }
+
+  // returns value of top stack element
+  peek() {
+
+  }
+}
