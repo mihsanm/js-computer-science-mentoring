52a078f59fc98c3a6729a32721a7640421db79d3
Yahor Simak <simak_yahor@mail.ru>
2017-10-30 23:54:29 +0300

    js-computer-science-mentoring/feature: add empty queue class

---
+++ queue/queue.js
@@ -0,0 +1,29 @@
+import Node from './node';
+
+export default class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // delete first element of queue
+  shift() {
+
+  }
+
+  // add last element of queue
+  unshift() {
+
+  }
+
+  // returns size of queue
+  size() {
+
+  }
+
+  // returns true if queue is empty, else returns false
+  isEmpty() {
+
+  }
+}
