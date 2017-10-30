59ca84cbb83f7ec8a79ddb2d4a4578a1deff4ee9
Yahor Simak <simak_yahor@mail.ru>
2017-10-31 00:13:55 +0300

    js-computer-science-mentoring/feature: fix queue unshifgr method tests

--- queue/queue.spec.js
+++ queue/queue.spec.js
@@ -10,30 +10,29 @@ describe('Queue', () => {
         })
     });
 
-    describe('#push', () => {
+    describe('#unshift', () => {
         it('should assign new node as _top of queue', () => {
-            const value = 10;
+            const value = 144;
 
             const queue = new queue();
 
-            queue.push(value);
+            queue.unshift(value);
 
-            expect(queue._top).to.be.an.instanceof(Node);
-            expect(queue._top.value).to.equal(10);
+            expect(queue.head).to.be.an.instanceof(Node);
+            expect(queue.head.value).to.equal(144);
         });
 
         it('should set new node as _top of queue and add new value', () => {
             const queue = new Queue();
 
-            queue.push(50);
-            queue.push(100);
-            queue.push(51);
-            queue.push(422);
-            queue.push(55);
-            queue.push(112);
+            queue.unshift(50);
+            queue.unshift(100);
+            queue.unshift(51);
+            queue.unshift(422);
 
-            expect(queue.length).to.equal(6);
-            expect(queue._top.value).to.equal(112);
+            expect(queue.length).to.equal(4);
+            expect(queue.head.value).to.equal(50);
+            expect(queue.tail.value).to.equal(422);
         });
 
     });
